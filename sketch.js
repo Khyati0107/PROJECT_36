4947a2346932556d236c11adf27a8dde0559d189
Khyati0107 <78732262+Khyati0107@users.noreply.github.com>
2021-06-05 15:48:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 var dog,sadDog,happyDog, database;
 var foodS,foodStock;
-var addFood;
+var fedTime,lastFed;
+var feed,addFood;
 var foodObj;
-var feed, lastFed
 
 function preload(){
 sadDog=loadImage("Dog.png");
@@ -21,11 +21,10 @@ function setup() {
   dog=createSprite(800,200,150,150);
   dog.addImage(sadDog);
   dog.scale=0.15;
-
-  //create feed the dog button here
-  addfeedDog=createButton("Feed Dog");
-  addfeedDog.position(880,95);
-  addfeedDog.mousePressed(addfeedDog);
+  
+  feed=createButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
 
   addFood=createButton("Add Food");
   addFood.position(800,95);
@@ -36,22 +35,21 @@ function setup() {
 function draw() {
   background(46,139,87);
   foodObj.display();
- 
-  feedTime = database.ref('feedTime')
-  feedTime.on ("value",function(data){
-    lastFed = data.val();
-  })
- 
-  fill (255,255,254)
-  textSize(15)
 
-  if (lastFed >= 12){
-    text ("last Feed : " + lastFed%12 +"PM" , 350,30)
-  } else if (lastFed == 0){
-    text ("last Feed : 12 AM", 350,30)
-  }else {
-    text ( "Last Feed :"+lastFed+ "AM", 350,30)
-  }
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
+ 
+  fill(255,255,254);
+  textSize(15);
+  if(lastFed>=12){
+    text("Last Feed : "+ lastFed%12 + " PM", 350,30);
+   }else if(lastFed==0){
+     text("Last Feed : 12 AM",350,30);
+   }else{
+     text("Last Feed : "+ lastFed + " AM", 350,30);
+   }
  
   drawSprites();
 }
@@ -62,19 +60,21 @@ function readStock(data){
   foodObj.updateFoodStock(foodS);
 }
 
+
+//function to update food stock and last fed time
 function feedDog(){
   dog.addImage(happyDog);
-
+  
   var food_stock_val = foodObj.getFoodStock();
-  if (food_stock_val <= 0){
-    foodObj.updateFoodStock (food_stock_val *0)
-  } else {
-    foodObj.updateFoodStock(food_stock_val -1)
+  if(food_stock_val <= 0){
+      foodObj.updateFoodStock(food_stock_val *0);
+  }else{
+      foodObj.updateFoodStock(food_stock_val -1);
   }
-
-  database.ref ('/').update({
-    Food: foodObj.getFoodStock(),
-    FeedTime: hour()
+  
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour()
   })
 }
 
